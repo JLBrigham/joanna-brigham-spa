81578703a3f85bf362efa0ed491dc231447bb9c0
Joanna Brigham <joannabrigham@hotmail.co.uk>
2022-02-10 10:26:45 +0100

    Basic navbar created

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,28 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <a href="/">Joanna Brigham</a>
+        </li>
+        <li>
+          <a href="/skills">Skills</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li>
+          <a href="/experience">Experience</a>
+        </li>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+        <li>
+          <a href="/hobbies">Hobbies</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
