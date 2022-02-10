8803c06f4fabc053129fe1ad7d100d1887e08e6f
Joanna Brigham <joannabrigham@hotmail.co.uk>
2022-02-10 10:17:28 +0100

    Main layout created

---
+++ layouts/main.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar/navbar.js";
+
+function Main() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="content"></div>
+    </div>
+  );
+}
+
+export default Main;
